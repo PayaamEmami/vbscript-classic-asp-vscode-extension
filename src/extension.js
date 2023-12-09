099650a982652af39ffc172258614461d4d980b0
Payaam Emami <contact@payaame.com>
2023-12-09 14:05:07 -0800

    added error handling and removed grammars

--- src/extension.js
+++ src/extension.js
@@ -6,47 +6,62 @@ class VBScriptDefinitionProvider {
     }
 
     async provideDefinition(document, position, token) {
-        const word = document.getText(document.getWordRangeAtPosition(position));
-        const searchPattern = new RegExp(`(Function|Sub)\\s+${word}\\b`, 'i');
+        try {
+            const word = document.getText(document.getWordRangeAtPosition(position));
+            const searchPattern = new RegExp(`(Function|Sub)\\s+${word}\\b`, 'i');
 
-        // Check cache first
-        const cacheKey = document.uri.toString() + ':' + word;
-        if (this.definitionCache.has(cacheKey)) {
-            return this.definitionCache.get(cacheKey);
-        }
+            // Check cache first
+            const cacheKey = document.uri.toString() + ':' + word;
+            if (this.definitionCache.has(cacheKey)) {
+                return this.definitionCache.get(cacheKey);
+            }
 
-        // Search in current document
-        let definitionLocation = await this.searchInDocument(document, searchPattern);
-        if (definitionLocation) {
+            // Search in current document
+            let definitionLocation = await this.searchInDocument(document, searchPattern);
+            if (definitionLocation) {
+                this.definitionCache.set(cacheKey, definitionLocation);
+                return definitionLocation;
+            }
+
+            // Search in other files
+            definitionLocation = await this.searchInWorkspace(searchPattern, document);
             this.definitionCache.set(cacheKey, definitionLocation);
             return definitionLocation;
+        } catch (error) {
+            console.error('Error providing definition:', error);
         }
-
-        // Search in other files
-        definitionLocation = await this.searchInWorkspace(searchPattern, document);
-        this.definitionCache.set(cacheKey, definitionLocation);
-        return definitionLocation;
     }
 
-
     async searchInDocument(document, pattern) {
-        for (let i = 0; i < document.lineCount; i++) {
-            let lineText = document.lineAt(i).text;
-            if (pattern.test(lineText)) {
-                return new vscode.Location(document.uri, new vscode.Position(i, 0));
+        try {
+            for (let i = 0; i < document.lineCount; i++) {
+                let lineText = document.lineAt(i).text;
+                if (pattern.test(lineText)) {
+                    return new vscode.Location(document.uri, new vscode.Position(i, 0));
+                }
             }
+        } catch (error) {
+            console.error('Error searching in document:', error);
         }
         return null;
     }
 
     async searchInWorkspace(pattern, currentDocument) {
-        const files = await vscode.workspace.findFiles('**/*.{html,asp}', '**/node_modules/**');
-        for (const file of files) {
-            if (file.fsPath === currentDocument.uri.fsPath) continue;
+        try {
+            const files = await vscode.workspace.findFiles('**/*.{html,asp}', '**/node_modules/**');
+            for (const file of files) {
+                if (file.fsPath === currentDocument.uri.fsPath) continue;
 
-            const document = await vscode.workspace.openTextDocument(file);
-            const location = await this.searchInDocument(document, pattern);
-            if (location) return location;
+                try {
+                    const document = await vscode.workspace.openTextDocument(file);
+                    const location = await this.searchInDocument(document, pattern);
+                    if (location) return location;
+                } catch (innerError) {
+                    console.error(`Error opening or searching file ${file.fsPath}:`, innerError);
+                }
+            }
+        } catch (error) {
+            console.error('Error searching in workspace:', error);
         }
         return null;
     }
