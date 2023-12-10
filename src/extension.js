5fd89f8eceebed470f926059574ed368143b1d45
Payaam Emami <contact@payaame.com>
2023-12-10 14:04:59 -0800

    fixed issues with quick pick

--- src/extension.js
+++ src/extension.js
@@ -2,17 +2,15 @@ const vscode = require('vscode');
 
 class VBScriptDefinitionProvider {
     constructor() {
-        this.definitionCache = new Map(); // Cache to store search results
+        this.definitionCache = new Map();
     }
 
     async provideDefinition(document, position, token) {
         try {
             const word = document.getText(document.getWordRangeAtPosition(position));
             const searchPattern = new RegExp(`(Function|Sub)\\s+${word}\\b`, 'i');
-
             const cacheKey = document.uri.toString() + ':' + word;
 
-            // Attempt to get a cached definition only if there's a single definition
             if (this.definitionCache.has(cacheKey)) {
                 const cachedDefinitions = this.definitionCache.get(cacheKey);
                 if (cachedDefinitions.length === 1) {
@@ -26,25 +24,25 @@ class VBScriptDefinitionProvider {
             }
 
             if (definitions.length === 1) {
-                // Cache only if there's a single definition
                 this.definitionCache.set(cacheKey, definitions);
                 return definitions[0];
             } else if (definitions.length > 1) {
-                const picks = definitions.map(def => {
-                    const lineText = def.document.lineAt(def.location.range.start.line).text.trim();
+                const picks = await Promise.all(definitions.map(async (location) => {
+                    const defDocument = await vscode.workspace.openTextDocument(location.uri);
+                    const lineText = defDocument.lineAt(location.range.start.line).text.trim();
                     return {
-                        label: `${def.location.uri.fsPath}:${def.location.range.start.line}`,
-                        detail: lineText, // Function signature snippet
-                        location: def.location
+                        label: `${location.uri.fsPath}:${location.range.start.line + 1}`,
+                        detail: lineText,
+                        location: location
                     };
-                });
+                }));
 
                 const selected = await vscode.window.showQuickPick(picks, {
                     placeHolder: 'Multiple definitions found. Select one to navigate to.'
                 });
 
                 if (selected) {
-                    return selected.location; // Ensure this is a vscode.Location object
+                    return selected.location;
                 }
             }
         } catch (error) {
@@ -53,21 +51,20 @@ class VBScriptDefinitionProvider {
     }
 
     async searchInDocument(document, pattern) {
-        let results = [];
+        let locations = [];
         try {
             for (let i = 0; i < document.lineCount; i++) {
                 let lineText = document.lineAt(i).text;
                 if (pattern.test(lineText)) {
                     const location = new vscode.Location(document.uri, new vscode.Position(i, 0));
-                    results.push({ document, location }); // Store document along with location
+                    locations.push(location);
                 }
             }
         } catch (error) {
             console.error('Error searching in document:', error);
         }
-        return results;
+        return locations;
     }
-
 
     async searchInWorkspace(pattern, currentDocument) {
         let locations = [];
@@ -94,9 +91,8 @@ class VBScriptDefinitionProvider {
 function activate(context) {
     let provider = new VBScriptDefinitionProvider();
     let disposable = vscode.languages.registerDefinitionProvider(['html', 'plaintext'], provider);
-
-    // Clear cache when files are changed
     let watcher = vscode.workspace.createFileSystemWatcher('**/*.{html,asp,vbs}');
+
     watcher.onDidChange(() => provider.clearCache());
     watcher.onDidCreate(() => provider.clearCache());
     watcher.onDidDelete(() => provider.clearCache());
