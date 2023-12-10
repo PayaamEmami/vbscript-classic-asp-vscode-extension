bfed5194c17d277647f3cd39a822a7a865555263
Payaam Emami <contact@payaame.com>
2023-12-10 04:10:06 -0800

    quick pick work in progresS

--- src/extension.js
+++ src/extension.js
@@ -10,60 +10,80 @@ class VBScriptDefinitionProvider {
             const word = document.getText(document.getWordRangeAtPosition(position));
             const searchPattern = new RegExp(`(Function|Sub)\\s+${word}\\b`, 'i');
 
-            // Check cache first
             const cacheKey = document.uri.toString() + ':' + word;
+
+            // Attempt to get a cached definition only if there's a single definition
             if (this.definitionCache.has(cacheKey)) {
-                return this.definitionCache.get(cacheKey);
+                const cachedDefinitions = this.definitionCache.get(cacheKey);
+                if (cachedDefinitions.length === 1) {
+                    return cachedDefinitions[0];
+                }
             }
 
-            // Search in current document
-            let definitionLocation = await this.searchInDocument(document, searchPattern);
-            if (definitionLocation) {
-                this.definitionCache.set(cacheKey, definitionLocation);
-                return definitionLocation;
+            let definitions = await this.searchInDocument(document, searchPattern);
+            if (definitions.length === 0) {
+                definitions = await this.searchInWorkspace(searchPattern, document);
             }
 
-            // Search in other files
-            definitionLocation = await this.searchInWorkspace(searchPattern, document);
-            this.definitionCache.set(cacheKey, definitionLocation);
-            return definitionLocation;
+            if (definitions.length === 1) {
+                // Cache only if there's a single definition
+                this.definitionCache.set(cacheKey, definitions);
+                return definitions[0];
+            } else if (definitions.length > 1) {
+                const picks = definitions.map(def => {
+                    const lineText = def.document.lineAt(def.location.range.start.line).text.trim();
+                    return {
+                        label: `${def.location.uri.fsPath}:${def.location.range.start.line}`,
+                        detail: lineText, // Function signature snippet
+                        location: def.location
+                    };
+                });
+
+                const selected = await vscode.window.showQuickPick(picks, {
+                    placeHolder: 'Multiple definitions found. Select one to navigate to.'
+                });
+
+                if (selected) {
+                    return selected.location; // Ensure this is a vscode.Location object
+                }
+            }
         } catch (error) {
             console.error('Error providing definition:', error);
         }
     }
 
     async searchInDocument(document, pattern) {
+        let results = [];
         try {
             for (let i = 0; i < document.lineCount; i++) {
                 let lineText = document.lineAt(i).text;
                 if (pattern.test(lineText)) {
-                    return new vscode.Location(document.uri, new vscode.Position(i, 0));
+                    const location = new vscode.Location(document.uri, new vscode.Position(i, 0));
+                    results.push({ document, location }); // Store document along with location
                 }
             }
         } catch (error) {
             console.error('Error searching in document:', error);
         }
-        return null;
+        return results;
     }
 
+
     async searchInWorkspace(pattern, currentDocument) {
+        let locations = [];
         try {
             const files = await vscode.workspace.findFiles('**/*.{html,asp,vbs}', '**/node_modules/**');
             for (const file of files) {
                 if (file.fsPath === currentDocument.uri.fsPath) continue;
 
-                try {
-                    const document = await vscode.workspace.openTextDocument(file);
-                    const location = await this.searchInDocument(document, pattern);
-                    if (location) return location;
-                } catch (innerError) {
-                    console.error(`Error opening or searching file ${file.fsPath}:`, innerError);
-                }
+                const document = await vscode.workspace.openTextDocument(file);
+                const fileLocations = await this.searchInDocument(document, pattern);
+                locations = locations.concat(fileLocations);
             }
         } catch (error) {
             console.error('Error searching in workspace:', error);
         }
-        return null;
+        return locations;
     }
 
     clearCache() {
